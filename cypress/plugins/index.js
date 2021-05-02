7df33e1cfa009d744e8aade6c503478bd3ab4c55
sasial-roblox <44125644+sasial-dev@users.noreply.github.com>
2021-05-02 13:23:46 +1000

    Update Cypress Testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,8 @@
 module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, args) => {
-    console.log(browser, args); // see what all is in here!
-
+  on('before:browser:launch', (browser, launchOptions) => {
     if (browser.name === 'chrome') {
-      args.push('--disable-gpu');
-      return args;
+      launchOptions.args.push('--disable-gpu');
+      return launchOptions;
     }
   });
 };
