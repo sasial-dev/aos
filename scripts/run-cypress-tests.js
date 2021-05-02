c54b6e30c4ace22a9776e80e1b0e78a2a3ea86b2
sasial-roblox <44125644+sasial-dev@users.noreply.github.com>
2021-05-02 16:07:54 +1000

    Update Cypress to not exit 1 with on Sucess

--- scripts/run-cypress-tests.js
+++ scripts/run-cypress-tests.js
@@ -1,7 +1,21 @@
 const cypress = require('cypress');
 const server = require('./start-server');
 
-cypress.run().then(({ failures }) => {
-  server.close();
-  process.exit(failures === 0 ? 0 : 1);
-});
+cypress
+  .run()
+  .then((result) => {
+    server.close();
+    if (result.failures) {
+      console.error('Could not execute tests');
+      console.error(result.message);
+      process.exit(result.failures);
+    }
+
+    // print test results and exit
+    // with the number of failed tests as exit code
+    process.exit(result.totalFailed);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
